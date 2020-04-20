2a3afdb4dcefb6a18801b5a48c0f626f01aac72b
Pratik Patil <38435193+pratik7368patil@users.noreply.github.com>
2020-04-20 19:47:16 +0530

    Done upto locating element on web browser

--- mysite/core/views.py
+++ mysite/core/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 import pyttsx3
 import webbrowser
 import nltk
-from datetime import date
+from datetime import date, datetime
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
@@ -40,21 +40,19 @@ def get_cmd(request):
 		# result = exter_book_quick_play(cmd)
 		# now do your work here and return the result to play.html
 		# ------------Default Query is---------------
-		# book bus for me from Mumbai to Pune bus type ordinary on 23 next month
+		# book bus for me from Mumbai to Pune on 23 next month at 13
 		tokenized = nltk.word_tokenize(cmd)
 		query = [word for (word, pos) in nltk.pos_tag(tokenized) if(pos[:2] == 'NN' or pos[:2] == 'CD' or pos[:2] == 'JJ')]
 		if query[0]+query[1] == 'bookbus': # if this is valid extract all data 
 			source_city = query[2]
-			des_city = query[3]
-			bus_type = query[6]
+			des_city = query[3] 
 			try:
-				booking_time = query[9]
-				time_am_or_pm = query[10]
+				booking_time = str(query[7]) + ":00"
 			except:
 				None
 
-			dd = query[7]
-			mm = query[8]
+			dd = query[4]
+			mm = query[5]
 			x = date.today()
 			yyyy = x.strftime("%Y")
 			if mm == 'next':
@@ -65,12 +63,12 @@ def get_cmd(request):
 						mm = x.replace(year=x.year + 1, month = 1)
 						yyyy = x.replace(year=x.year + 1).strftime("%Y")
 			else:
-				mm
+				None
 			engine = pyttsx3.init()
 			engine.say("Your Source City is "+source_city)
 			engine.say(" your destination city is "+des_city)
-			engine.say(" your bus type is "+bus_type)
 			engine.say(" and date is" +dd+" "+mm+ " "+yyyy)
+			engine.say("Relax and do your work.")
 			engine.runAndWait()
 			# scrap data from webpages of given url
 			driver = webdriver.Chrome("c:\Final Year Project\Chrome Driver\chromedriver.exe")
@@ -130,6 +128,7 @@ def get_cmd(request):
 			# remove all redudent data and filter it to get good results
 			content = driver.page_source
 			soup = BeautifulSoup(content, 'html.parser')
+			# use same info var for finding minimum at given time and for next process
 			info = soup.find_all('div', attrs={'class': 'clearfix row-one'})
 			name_ = []
 			type_ = []
@@ -139,8 +138,8 @@ def get_cmd(request):
 			for a in info:
 				n = a.find('div', attrs={'class': 'travels lh-24 f-bold d-color'})
 				name_.append(n.text)
-				t = a.find('div', attrs={'class': 'bus-type f-12 m-top-16 l-color'}) 
-				type_.append(t.text)
+				t = a.find('div', attrs={'class': 'bus-type f-12 m-top-16 l-color'})
+				type_.append(t.text.lower())
 				p = a.find('div', attrs={'class': 'seat-fare'})
 				p_with_text = p.text
 				p_without_text = [int(i) for i in p_with_text.split() if i.isdigit()]
@@ -151,19 +150,71 @@ def get_cmd(request):
 			df = pd.DataFrame({'Travels Name': name_, 'Bus Type': type_, 'Price': price_, 'Time': time_})
 			# data_file_path = os.path.dirname(os.path.realpath('./data/data_handling.csv'))
 			df.to_csv('data_handling.csv', index=False, encoding='utf-8')
-			driver.close()
+			# do not close browser use same for next process
 
-			# finding the average price of all buses to give good results 
-			csv_data = pd.read_csv('data_handling.csv')
-			all_ele = []
-			for row in csv_data.index:
-				all_ele.append(csv_data['Price'][row])
+			# making data frame from file
+			data = pd.read_csv('data_handling.csv', delimiter=',')
+			
+			# replaceing black sapces with underscore sign '_'
+			data.columns = [column.replace(" ", "_") for column in data.columns]
 
-			average_price =(sum(all_ele)//len(all_ele)) 
+			# filter out all buses at given time and suggest a bus having lowest price
+			bus_at_time = data[data['Time'] == booking_time]
+			min_bus_at_time = bus_at_time[bus_at_time['Price'] == bus_at_time['Price'].min()]
+			# filter out data according to time and bus type and price at once 
+			# if bus is not present at given time then consider bus according to time
+			# find the bus type at user time
+			# given_bus_type_at_time = data[data['Bus_Type'].dropna().str.contains(b_type.lower())]
+			# if len(given_bus_type_at_time) == 0:
+			# 	engine.say("Bus type not found at given time")
+			# 	engine.say("I will pick your bus by Time.")
+			# 	engine.runAndWait()
+			# 	result = min_bus_at_time
+			# else:
+			# 	min_given_bus_type_at_time = given_bus_type_at_time[given_bus_type_at_time["Price"] == given_bus_type_at_time["Price"].min()]
+			# 	engine.say("Bus found at minimum cost")
+			# 	engine.say("Now its time to book")
+			# 	result = min_given_bus_type_at_time
+			# consider minimum cost bus at given time
+			if len(min_bus_at_time) == 0:
+				engine.say("Bus not found now you have to do it by yourself")
+				engine.say("You can give me new information for booking")
+				engine.runAndWait()
+				result = "Not Found"
+			else:
+				engine.say("Bus found at your time")
+				engine.runAndWait()
+				result = min_bus_at_time
 
+			res = []
+			for index, row in result.iterrows():
+				res.append(row[0])
+				res.append(row[1])
+				res.append(row[2])
+				res.append(row[3])
+			engine.say("bus name is "+ res[0])
+			engine.runAndWait()
 
-		scrap_data = [['Testing Name','Testing','312','12:00'],['dummy','dum','123','1:00']]
-		return render(request, 'play.html', {'result':average_price,'scrap_data':scrap_data})
+			new_res = []
+			for i in res:
+				new_res.append(str(i).replace(" ",""))
+			# now match our extracted information on website and do further process
+			new_info = soup.find_all('div', attrs={'class': 'clearfix bus-item'})
+			# make counter to select bus on web browser
+			count = 0
+			for a in new_info:
+				count += 1
+				n = a.find('div', attrs={'class': 'travels lh-24 f-bold d-color'})
+				tm = a.find('div', attrs={'class': 'dp-time f-19 d-color f-bold'})
+				if n.text.replace(" ","") == new_res[0] and tm.text == new_res[3]:
+					break
+			v = "/html/body/section/div[2]/div[1]/div/div[2]/div[2]/div[2]/ul/div["+ str(count) +"]/li/div/div[2]/div[1]"
+			ele = driver.find_element_by_xpath(v)
+			driver.execute_script("arguments[0].click();", ele)
+
+		#scrap_data = [['Testing Name','Testing','312','12:00'],['dummy','dum','123','1:00']]
+		return render(request, 'play.html', {'res_name':res[0], 'res_type':res[1],'res_price': res[2],'res_time':res[3]})
+
 
 @login_required
 def secret_page(request):
@@ -178,3 +229,4 @@ def play(request):
 
 def how_work(request):
 	return render(request, 'how_work.html')
+
